01223e179e5212771585c212fac48fcee95e2e12
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-11 16:00:49 +0900

    [UPDATE] Change

--- MySQL_DML_DummyData.sql
+++ MySQL_DML_DummyData.sql
@@ -1,13 +1,13 @@
 use testdb_saturn;
 
 DELETE FROM attendance;
+DELETE FROM pre_absence;
 DELETE FROM activity;
 DELETE FROM student;
 DELETE FROM class;
 DELETE FROM club;
 DELETE FROM club_location;
 DELETE FROM teacher;
-DELETE FROM pre_absence;
 
 INSERT INTO teacher(id, pw, name) VALUES('aaa', 'd6f644b19812e97b5d871658d6d3400ecd4787faeb9b8990c1e7608288664be77257104a58d033bcf1a0e0945ff06468ebe53e2dff36e248424c7273117dac09', '김정은');
 INSERT INTO teacher(id, pw, name) VALUES('bbb', '5edc1c6a4390075a3ca27f4d4161c46b374b1c3b2d63f846db6fff0c513203c3ac3b14a24a6f09d8bf21407a4842113b5d9aa359d266299c3d6cf9e92db66dbe', '안소희');
