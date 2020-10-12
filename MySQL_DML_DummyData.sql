a5a2cfe2a1b99859ae774a827c1dcb7faea56eb6
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-12 12:57:13 +0900

    [UPDATE] gamil.com => gmail.com

--- MySQL_DML_DummyData.sql
+++ MySQL_DML_DummyData.sql
@@ -9,12 +9,12 @@ DELETE FROM club;
 DELETE FROM club_location;
 DELETE FROM teacher;
 
-INSERT INTO teacher(id, pw, name) VALUES('aaa@gamil.com', 'd6f644b19812e97b5d871658d6d3400ecd4787faeb9b8990c1e7608288664be77257104a58d033bcf1a0e0945ff06468ebe53e2dff36e248424c7273117dac09', '김정은');
-INSERT INTO teacher(id, pw, name) VALUES('bbb@gamil.com', '5edc1c6a4390075a3ca27f4d4161c46b374b1c3b2d63f846db6fff0c513203c3ac3b14a24a6f09d8bf21407a4842113b5d9aa359d266299c3d6cf9e92db66dbe', '안소희');
-INSERT INTO teacher(id, pw, name) VALUES('ccc@gamil.com', '2b83283b8caf7e952ad6b0443a87cd9ee263bc32c4d78c5b678ac03556175059679b4b8513b021b16a27f6d2a35484703129129f35b6cdfe418ef6473b1f8f23', '조규석');
-INSERT INTO teacher(id, pw, name) VALUES('ddd@gamil.com', '3ba2942ed1d05551d4360a2a7bb6298c2359061dc07b368949bd3fb7feca3344221257672d772ce456075b7cfa50fd7ce41eaefe529d056bf23dd665de668b78', '안희명');
-INSERT INTO teacher(id, pw, name) VALUES('eee@gamil.com', '4e83a3339059f0cc30910ae3ca1d01d854a0fefd2f277776e044daad540bbdd5c79033a1c2eb851dffd258194bd58f5657e93fa21b70b5180e7d0efc60040cd4', '서현철');
-INSERT INTO teacher(id, pw, name) VALUES('fff@gamil.com', 'd1d7e84d9049900299ee9c0b2c04b11bfa9a0437afc0bf03d8ce0e3fb8523919f13fa3a5130c5ba7987679c6f6945ca87655e746eb5345bdc8131298fa5a9b20', '엄영석');
+INSERT INTO teacher(id, pw, name) VALUES('aaa@gmail.com', 'd6f644b19812e97b5d871658d6d3400ecd4787faeb9b8990c1e7608288664be77257104a58d033bcf1a0e0945ff06468ebe53e2dff36e248424c7273117dac09', '김정은');
+INSERT INTO teacher(id, pw, name) VALUES('bbb@gmail.com', '5edc1c6a4390075a3ca27f4d4161c46b374b1c3b2d63f846db6fff0c513203c3ac3b14a24a6f09d8bf21407a4842113b5d9aa359d266299c3d6cf9e92db66dbe', '안소희');
+INSERT INTO teacher(id, pw, name) VALUES('ccc@gmail.com', '2b83283b8caf7e952ad6b0443a87cd9ee263bc32c4d78c5b678ac03556175059679b4b8513b021b16a27f6d2a35484703129129f35b6cdfe418ef6473b1f8f23', '조규석');
+INSERT INTO teacher(id, pw, name) VALUES('ddd@gmail.com', '3ba2942ed1d05551d4360a2a7bb6298c2359061dc07b368949bd3fb7feca3344221257672d772ce456075b7cfa50fd7ce41eaefe529d056bf23dd665de668b78', '안희명');
+INSERT INTO teacher(id, pw, name) VALUES('eee@gmail.com', '4e83a3339059f0cc30910ae3ca1d01d854a0fefd2f277776e044daad540bbdd5c79033a1c2eb851dffd258194bd58f5657e93fa21b70b5180e7d0efc60040cd4', '서현철');
+INSERT INTO teacher(id, pw, name) VALUES('fff@gmail.com', 'd1d7e84d9049900299ee9c0b2c04b11bfa9a0437afc0bf03d8ce0e3fb8523919f13fa3a5130c5ba7987679c6f6945ca87655e746eb5345bdc8131298fa5a9b20', '엄영석');
 
 INSERT INTO club_location(location, floor, priority) VALUES('2학년 1반', 3, 0);
 INSERT INTO club_location(location, floor, priority) VALUES('2학년 2반', 3, 1);
